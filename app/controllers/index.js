04c68e0e62be1c16c75dd532f8f19d92c2dcbc12
Garret Alfert <alfert@wevelop.de>
2017-07-28 12:10:55 +0200

    Storage address is coming via the API now

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,9 @@
 import Ember from 'ember';
-import config from '../config/environment';
 
-const { Controller, inject: { service }, computed, isEmpty } = Ember;
+const { Controller, inject: { service } } = Ember;
 
 export default Controller.extend({
 
-  currentUser: service(),
-
-  username: Ember.computed.alias('currentUser.user.username'),
-
-  userAddress: computed('username', function() {
-    const username = this.get('username');
-
-    if (isEmpty(username)) {
-      return 'unknown';
-    }
-
-    return `${this.get('username')}@${config.baseDomain}`;
-  })
+  currentUser: service()
 
 });
