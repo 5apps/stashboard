cbda1b7c27aea10a0f8cd851ebf722fecc4d060c
Garret Alfert <alfert@wevelop.de>
2017-07-28 21:41:12 +0200

    Use platform CSS

--- app/controllers/apps.js
+++ app/controllers/apps.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
-const { Controller, inject: { service } } = Ember;
+const { Controller, inject: { service }, computed } = Ember;
 
 export default Controller.extend({
 
   currentUser: service(),
+
+  appsCount: computed('model.[]', function() {
+    return this.get('model.length');
+  }),
 
   actions: {
     revokeAccess(auth) {
