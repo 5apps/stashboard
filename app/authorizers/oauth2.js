c0b21fb9cd82c2f17339887a05364e088b5dabca
Garret Alfert <alfert@wevelop.de>
2018-01-27 14:27:13 +0100

    Adapt auth header to platform requirement

--- app/authorizers/oauth2.js
+++ app/authorizers/oauth2.js
@@ -1,3 +1,16 @@
+import Ember from 'ember';
 import OAuth2Bearer from 'ember-simple-auth/authorizers/oauth2-bearer';
 
-export default OAuth2Bearer.extend();
+const { isEmpty } = Ember;
+
+export default OAuth2Bearer.extend({
+
+  authorize(data, block) {
+    const accessToken = data['access_token'];
+
+    if (!isEmpty(accessToken)) {
+      block('Authorization', `OAuth ${accessToken}`);
+    }
+  }
+
+});
