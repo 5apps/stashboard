a29ff9f82d8ce5745e886af380a1d6da46db9884
Sebastian Kippe <sebastian@kip.pe>
2018-02-02 21:50:30 +0000

    Basic new layout, fonts, colors

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/bourbon/app/assets/stylesheets'
+      ]
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
