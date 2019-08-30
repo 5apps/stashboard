04a14038d1e86631e3cfa80696c293f7705d86fe
Sebastian Kippe <sebastian@kip.pe>
2019-08-30 15:13:17 +0200

    Update Ember
    No codemods run yet

--- testem.js
+++ testem.js
@@ -9,13 +9,19 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
-      mode: 'ci',
-      args: [
+      ci: [
         // --no-sandbox is needed when running Chrome inside a container
+<<<<<<< HEAD
         process.env.TRAVIS || process.env.CI ? '--no-sandbox' : null,
 
         '--disable-gpu',
+=======
+        process.env.CI ? '--no-sandbox' : null,
+>>>>>>> d4c092d... message
         '--headless',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ].filter(Boolean)
