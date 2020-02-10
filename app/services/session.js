9e17984751edac2bdff4d88d3da6dae8b818ef85
Garret Alfert <alfert@wevelop.de>
2020-02-10 18:51:11 +0100

    Use session-storage on no-remember state param
    Closes #37
    
    When the OAuth provider adds a state param of "no-remember", instead of saving the session to localStorage, we use the session storage that gets cleared on Browser restarts.

--- app/services/session.js
+++ app/services/session.js
@@ -1,4 +1,6 @@
+import { getOwner } from '@ember/application';
 import SessionService from 'ember-simple-auth/services/session';
+import SessionStorage from 'ember-simple-auth/session-stores/session-storage';
 import config from '../config/environment';
 import window from 'ember-window-mock';
 
@@ -6,7 +8,7 @@ export default SessionService.extend({
 
   userTriggeredSignout: false,
 
-  authenticateWithImplicitGrant() {
+  authenticateWithImplicitGrant () {
     const clientId = 'storage-frontend';
     const redirectURI = `${window.location.origin}${config.rootURL}callback`;
     const responseType = 'token';
@@ -19,6 +21,17 @@ export default SessionService.extend({
       + `&scope=${scope}`;
 
     window.location.replace(oauthURL);
+  },
+
+  authenticate (authenticator, hash) {
+    // don't persist session data when user didn't select "remember me"
+    if (hash && hash.state === 'no-remember') {
+      const owner = getOwner(this);
+      const store = SessionStorage.create(owner.ownerInjection(), { _isFastBoot: false });
+      this.session.set('store', store);
+    }
+
+    return this._super(...arguments);
   }
 
 });
